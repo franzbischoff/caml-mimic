5c6faea84556751a3e20a8931e0a3d73263691f3
Francisco Bischoff <franzbischoff@gmail.com>
2021-07-24 09:10:36 +0000

    notebook mimic3 ok

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@ EMBEDDING_SIZE = 100
 MAX_LENGTH = 2500
 
 #where you want to save any models you may train
-MODEL_DIR = './saved_models/'
+MODEL_DIR = '../saved_models/'
 
-DATA_DIR = './mimicdata/'
-MIMIC_3_DIR = './mimicdata/mimic3'
-MIMIC_2_DIR = './mimicdata/mimic2'
+DATA_DIR = '../mimicdata'
+MIMIC_3_DIR = '../mimicdata/mimic3'
+MIMIC_2_DIR = '../mimicdata/mimic2'
